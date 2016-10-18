19ef163337bdd64162b030ceadb42d914ca63296
Dustin Jensen <dustincjensen@gmail.com>
2016-10-18 13:13:10 -0600

    Added the project files.
    This includes the dll for Hotkeys. This was code I found elsewhere that allows the registering of global hotkeys. I have modified it to work better, but since I don't recall the original source currently, it is included as a dll instead of the base code.

---
+++ Snapper/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        protected override void OnStateChanged(EventArgs args)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+            }
+            base.OnStateChanged(args);
+        }
+        
+
+        private Hotkeys.GlobalHotKey _numpad1_HotKey;
+        private Hotkeys.GlobalHotKey _numpad2_HotKey;
+        private Hotkeys.GlobalHotKey _numpad3_HotKey;
+        private Hotkeys.GlobalHotKey _numpad4_HotKey;
+        private Hotkeys.GlobalHotKey _numpad5_HotKey;
+        private Hotkeys.GlobalHotKey _numpad6_HotKey;
+        private Hotkeys.GlobalHotKey _numpad7_HotKey;
+        private Hotkeys.GlobalHotKey _numpad8_HotKey;
+        private Hotkeys.GlobalHotKey _numpad9_HotKey;
+        private Hotkeys.GlobalHotKey _numpad0_HotKey;
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            _LoadHotkeys();
+            _SetupNotifyIcon();
+
+            WindowState = WindowState.Minimized;
+        }
+
+        private void _LoadHotkeys()
+        {
+            _numpad1_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad1, this);
+            _numpad2_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad2, this);
+            _numpad3_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad3, this);
+            _numpad4_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad4, this);
+            _numpad5_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad5, this);
+            _numpad6_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad6, this);
+            _numpad7_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad7, this);
+            _numpad8_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad8, this);
+            _numpad9_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad9, this);
+            _numpad1_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad2_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad3_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad4_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad5_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad6_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad7_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad8_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+            _numpad9_HotKey.HotKeyPressed += _MoveWindowOnHotKeyPressed;
+
+            _numpad0_HotKey = new Hotkeys.GlobalHotKey(ModifierKeys.Control | ModifierKeys.Alt, Key.NumPad0, this);
+            _numpad0_HotKey.HotKeyPressed += _ToggleWindowMonitorOnHotKeyPressed;
+        }
+
+        private void _ToggleWindowMonitorOnHotKeyPressed(Hotkeys.GlobalHotKey globalHotKey)
+        {
+            MoveActiveWindow.ToggleWindowMonitor();
+        }
+
+        private void _MoveWindowOnHotKeyPressed(Hotkeys.GlobalHotKey globalHotKey)
+        {
+            MoveActiveWindow.MoveWindow(globalHotKey);
+        }
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/system.windows.forms.notifyicon.contextmenu(v=vs.110).aspx
+        /// </summary>
+        private void _SetupNotifyIcon()
+        {
+            var contextMenu = new System.Windows.Forms.ContextMenu();
+            var menuItem = new System.Windows.Forms.MenuItem();
+
+            // Initialize contextMenu1
+            contextMenu.MenuItems.AddRange(new[] { menuItem });
+
+            // Initialize menuItem1
+            menuItem.Index = 0;
+            menuItem.Text = "O&pen";
+            menuItem.Click += delegate
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
+
+            var notificationIcon = new NotifyIcon
+            {
+                Icon = new Icon("img/Window.ico"),
+                Visible = true,
+                ContextMenu = contextMenu
+            };
+
+            notificationIcon.DoubleClick += delegate
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
+        }
+    }
+}
---
+++ Snapper/MoveActiveWindow.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Hotkeys;
+
+namespace Snapper
+{
+    public static class MoveActiveWindow
+    {
+        /// <summary>
+        /// Changes the size, position, and Z order of a child, pop-up, or top-level window. 
+        /// These windows are ordered according to their appearance on the screen. 
+        /// The topmost window receives the highest rank and is the first window in the Z order.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window.</param>
+        /// <param name="hWndInsertAfter">A handle to the window to precede the positioned window in the Z order.</param>
+        /// <param name="x">New left side.</param>
+        /// <param name="y">New top side.</param>
+        /// <param name="cx">New width.</param>
+        /// <param name="cy">New height.</param>
+        /// <param name="wFlags">The window sizing and positioning flags.</param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out Rectangle lpRect);
+
+
+        private static class SWP
+        {
+            public static int NOSIZE = 1;
+            public static int SHOWWINDOW = 0x0040;
+        }
+
+        public static void ToggleWindowMonitor()
+        {
+            IntPtr handle = GetForegroundWindow();
+            if (handle == IntPtr.Zero)
+            {
+                // TODO: setup exception handling
+                return;
+            }
+
+            var allScreens = Screen.AllScreens;
+            var currentScreen = Screen.FromHandle(handle);
+            Rectangle workingArea = currentScreen.WorkingArea;
+            var activeWindow = _GetActiveWindowDimensions(handle);
+
+            var minX = 0;
+            var maxX = 0;
+            foreach (var screen in allScreens)
+            {
+                Rectangle wa = screen.WorkingArea;
+                if (wa.X < minX)
+                {
+                    minX = wa.X;
+                }
+                if (wa.X + wa.Width > maxX)
+                {
+                    maxX = wa.X + wa.Width;
+                }
+            }
+
+            // TODO test with 3 monitors...
+            // Where 0 is primary...
+            // [-2, -1,  0]
+            // [-1,  0,  1]
+            // [ 0,  1,  2]
+            var newX = (activeWindow.X+7) + workingArea.Width < maxX
+                ? activeWindow.X + workingArea.Width
+                : activeWindow.X - workingArea.Width;
+
+            SetWindowPos(handle, 0, newX, activeWindow.Y, 0,0, SWP.NOSIZE | SWP.SHOWWINDOW);
+        }
+
+        public static void MoveWindow(GlobalHotKey hotKey)
+        {
+            IntPtr handle = GetForegroundWindow();
+            if (handle == IntPtr.Zero)
+            {
+                // TODO: setup exception handling
+                return;
+            }            
+
+            Rectangle workingArea = Screen.FromHandle(handle).WorkingArea;
+            var windowDimension = _GetActiveWindowDimensions(handle);
+            var positionDimension = _GetWindowPositionPlusDimensions(hotKey, workingArea, windowDimension);
+                                   
+            SetWindowPos(handle, 0, 
+                positionDimension.Left - 7, 
+                positionDimension.Top, 
+                positionDimension.Width + 14,
+                positionDimension.Height + 7, 
+                SWP.SHOWWINDOW);
+        }
+
+        private static Rectangle _GetActiveWindowDimensions(IntPtr handle)
+        {
+            Rectangle dim;
+            if (!GetWindowRect(handle, out dim))
+            {
+                throw new Exception("Failed to retrieve current window dimensions.");
+            }
+
+            return new Rectangle
+            {
+                X = dim.Left,
+                Y = dim.Top,
+                Width = dim.Right - (dim.Left*2),
+                Height = dim.Bottom - (dim.Top*2)
+            };
+        }
+
+        struct PositionDimension
+        {
+            public int Top;
+            public int Left;
+            public int Width;
+            public int Height;
+        }
+
+        private static PositionDimension _GetWindowPositionPlusDimensions(GlobalHotKey hotKey, Rectangle workingArea, Rectangle windowDimensions)
+        {
+            var positionDimension = new PositionDimension
+            {
+                Top = _GetTop(hotKey, workingArea),
+                Height = _GetHeight(hotKey, workingArea),
+                // adding the workingArea.X handles multi monitors situations.
+                Left = _GetLeft(hotKey, workingArea, windowDimensions) + workingArea.X,
+                Width = _GetWidth(hotKey, workingArea, windowDimensions)
+            };
+            return positionDimension;
+        }
+
+        private static int _GetTop(GlobalHotKey hotKey, Rectangle workingArea)
+        {
+            if (hotKey.Key == Key.NumPad1 || hotKey.Key == Key.NumPad2 || hotKey.Key == Key.NumPad3)
+            {
+                return workingArea.Height/2;
+            }
+            return 0;
+        }
+
+        private static int _GetHeight(GlobalHotKey hotKey, Rectangle workingArea)
+        {
+            if (hotKey.Key == Key.NumPad4 || hotKey.Key == Key.NumPad5 || hotKey.Key == Key.NumPad6)
+            {
+                return workingArea.Height;
+            }
+            return workingArea.Height / 2;            
+        }
+
+        private static int _GetLeft(GlobalHotKey hotKey, Rectangle workingArea, Rectangle windowDimensions)
+        {
+            var third = workingArea.Width / 3;
+            var half = workingArea.Width / 2;
+
+            if (hotKey.Key == Key.NumPad1 || hotKey.Key == Key.NumPad4 || hotKey.Key == Key.NumPad7)
+            {
+                return 0;
+            }
+            if (hotKey.Key == Key.NumPad2 || hotKey.Key == Key.NumPad5 || hotKey.Key == Key.NumPad8)
+            {
+                if (windowDimensions.Width >= workingArea.Width)
+                {
+                    return third;
+                }
+                if (windowDimensions.Width < half)
+                {
+                    return third/2;
+                }
+                return 0;
+            }
+            if (hotKey.Key == Key.NumPad3 || hotKey.Key == Key.NumPad6 || hotKey.Key == Key.NumPad9)
+            {
+                if (windowDimensions.Width >= third*2)
+                {
+                    return third*2;
+                }
+                if (windowDimensions.Width < half)
+                {
+                    return half;
+                }
+                return third;
+            }
+
+            return -1;
+        }
+
+        private static int _GetWidth(GlobalHotKey hotKey, Rectangle workingArea, Rectangle windowDimensions)
+        {
+            var third = workingArea.Width / 3;
+            var half = workingArea.Width / 2;
+
+            if (hotKey.Key == Key.NumPad2 || hotKey.Key == Key.NumPad5 || hotKey.Key == Key.NumPad8)
+            {            
+                if (windowDimensions.Width >= workingArea.Width)
+                {
+                    return third;
+                }
+                if (windowDimensions.Width < half)
+                {
+                    return third*2;
+                }
+                return workingArea.Width;
+            }
+
+
+            if (windowDimensions.Width >= third * 2)
+            {
+                return third;
+            }
+            if (windowDimensions.Width < half)
+            {
+                return half;
+            }
+            return third * 2;            
+        }
+    }
+}
